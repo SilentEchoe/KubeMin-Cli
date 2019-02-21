07e1504c838692dd5eecdabe86b6b8af01ec080b
kai_wang <871779972@qq.com>
2019-02-21 11:21:40 +0800

    add shutil

--- Demo1.py
+++ Demo1.py
@@ -1,7 +1,38 @@
 from io import StringIO
+import os
+
 f = StringIO()
 f.write('hello')
 print(f.getvalue())
+
+# 操作系统 posix为MAC OS X 或 Linux ，Unix . Nt则为WINDOWS
+os.name
+# 系统环境变量
+print(os.environ)
+
+print(os.environ.get('PATH'))
+
+print(os.environ.get('x', 'default'))
+
+# 查看当前目录的绝对路径
+_path = os.path.abspath('.')
+# 在某个目录下创建一个新目录，首先把新目录的完整路径表示出来
+_testpath = os.path.join('D:\Python_git', 'testdir')
+# 创建一个文件夹
+# os.mkdir(_testpath)
+# 删除一个文件夹
+# os.rmdir(_testpath)
+
+# 把两个路径合成一个时，要通过 os.path.join() ， 可以正确处理不同操作系统的路径分割符
+# 拆封路径的时候要使用 os.path.split()
+print(os.path.split(_testpath))
+
+os.path.splitext('/path/to/file.txt')
+print(os.path.splitext('/path/to/file.txt'))
+
+
+
+
 
 
 
