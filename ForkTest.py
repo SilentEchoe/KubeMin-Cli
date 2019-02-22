77ae20320300df041f8914f93d972110845128c5
kai_wang <871779972@qq.com>
2019-02-22 11:30:11 +0800

    add multiprocess

--- ForkTest.py
+++ ForkTest.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# 使用标准UTF-8编码
+
 from multiprocessing import Process
 from multiprocessing import Pool
 import subprocess
@@ -46,6 +49,12 @@ if __name__=='__main__':
     print('All subprocesses done')
 
 
-print('$ nslookup www.python.org')
-r = subprocess.call(['nslookup', 'www.python.org'])
-print('Exit code:', r)
+# print('$ nslookup www.python.org')
+# r = subprocess.call(['nslookup', 'www.python.org'])
+# print('Exit code:', r)
+
+print('$ nslookup')
+p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+print(output.decode('utf-8'))
+print('Exit code:', p.returncode)
