5e83425fd7d9618cbaf9373467c5b1923b686cde
kai_wang <871779972@qq.com>
2019-04-24 16:49:11 +0800

    添加测试def

--- Test/__init__.py
+++ Test/__init__.py
@@ -1,8 +1,6 @@
-import socket
 
-# AF_INET指定使用IPV4
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('www.sina.com.cn', 80))
-# 发送数据
 
+def application(environ, start_response):
+    start_response('200 ok', [('Content-Type', 'text/html')])
+    return [b'<h1>Hello,Web!</h1>>']
 
