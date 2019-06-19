d4ad1c8b2b4052360cb98b4845cb2a2a3516aa90
kai_wang <871779972@qq.com>
2019-06-19 16:21:19 +0800

    feat:构建图像

--- Test/pygameTest.py
+++ Test/pygameTest.py
@@ -3,8 +3,11 @@ import pygame
 
 def main():
     pygame.init()
-    scree = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption('魂斗罗')
+    screen.fill((242, 242, 242))
+    pygame.draw.circle(screen, (255, 0, 0), (100, 100), 30, 0)
+    pygame.display.flip()
     running = True
     with running:
         for event in pygame.event.get():
