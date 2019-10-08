cc07c325ace671014fac5f5ffc37def95193107f
kai_wang <871779972@qq.com>
2019-10-08 14:57:01 +0800

    装饰器

--- Test/Gui.py
+++ Test/Gui.py
@@ -1,20 +1,27 @@
-import subprocess
-import Tkinter as tk
-from tkMessageBox import *
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import tkinter
 
-p = subprocess.call\
-    ('mono -v',shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+root = tkinter.Tk()
+root.title('应用程序窗口')  # 窗口标题
+root.resizable(False, False)  # 固定窗口大小
+windowWidth = 800  # 获得当前窗口宽
+windowHeight = 500  # 获得当前窗口高
+screenWidth, screenHeight = root.maxsize()  # 获得屏幕宽和高
+geometryParam = '%dx%d+%d+%d' % (
+windowWidth, windowHeight, (screenWidth - windowWidth) / 2, (screenHeight - windowHeight) / 2)
+root.geometry(geometryParam)  # 设置窗口大小及偏移坐标
+root.wm_attributes('-topmost', 1)  # 窗口置顶
 
+# label文本
+label_text = tkinter.Label(root, text='文本');
+label_text.pack();
 
-root = tk.Tk()
+# label图片
+img_gif = tkinter.PhotoImage(file='18.gif')
+label_img = tkinter.Label(root, image=img_gif)
+label_img.pack()
 
-
-def on_closing():
-    if showwarning("Quit", "Do you want to quit?"):
-        root.destroy()
-
-
-root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
 
 
