8c42bb07a465018a0fdba39ab45c69a86c659e4e
kai_wang <871779972@qq.com>
2020-05-08 10:26:55 +0800

    添加pb

--- data.go
+++ data.go
@@ -1,1 +1,21 @@
-package LearningNotes_GoMicro
+package main
+
+import "LearningNotes-GoMicro/pb"
+
+/*var employees = []pb.Employee{
+	Id : '1',
+	No: 1994,
+	FirstName :"Chandler",
+	LastName:"Bing",
+	MonthSalay : &pb.MonthSalary{Basic:5000,Bonus:125.5,},
+	Status:pb.EmployeeStatus_NORMAL,
+
+}*/
+
+var employees = []pb.Employee{
+	Id : 1,
+
+}
+
+
+
