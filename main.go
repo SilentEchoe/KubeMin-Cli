eb18db43dece2e951d00055d17f35faa9ea8b8c3
kai_wang <871779972@qq.com>
2020-05-08 15:00:27 +0800

    添加pem

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"LearningNotes-GoMicro/pb"
-	"context"
+	"golang.org/x/net/context"
+	"log"
+	"net"
 )
 
+const  port =  ":5001"
+
+
+
 func main()  {
-	
+	Listen,err := net.Listen("tcp", port)
+	if err!=nil {
+		log.Fatalln(err.Error())
+	}
+
+
+
+
 }
 
 type employeeService struct {}
@@ -14,3 +27,17 @@ type employeeService struct {}
 func (s *employeeService) GetByNo(context.Context, *pb.GetByNoRequest) (*pb.EmployeeResponse, error)  {
 	return  nil , nil
 }
+
+func (s *employeeService) GetAll(request *pb.GetAllRequest,server pb.EmployeeService_SaveAllServer) error  {
+	return  nil
+}
+
+func (s *employeeService)AddPhoto(pb.EmployeeService_SaveAllServer)  error {
+	return  nil
+}
+
+func (s *employeeService) Save (context.Context, *pb.EmployeeRequest)(*pb.EmployeeResponse,error) {
+	return  nil,nil
+}
+
+
