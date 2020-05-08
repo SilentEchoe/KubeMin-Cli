801e22ab8c7b8f324b40a8e3408b5a012dabfc2e
kai_wang <871779972@qq.com>
2020-05-08 15:08:32 +0800

    添加服务器端口监听

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"LearningNotes-GoMicro/pb"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 )
@@ -16,25 +18,39 @@ func main()  {
 	if err!=nil {
 		log.Fatalln(err.Error())
 	}
+	creds, err := credentials.NewClientTLSFromFile("cert.pem","ket.pem")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	options := []grpc.ServerOption{grpc.Creds(creds)}
+	server := grpc.NewServer(options ...)
+	pb.RegisterEmployeeServiceServer(server, new(employeeService))
 
 
+	log.Println("Grpc Server started...")
+	server.Serve(Listen)
 
 
 }
 
 type employeeService struct {}
 
+func (s *employeeService) GetAll(context.Context, *pb.GetAllRequest) (*pb.EmployeeResponse, error) {
+	panic("implement me")
+}
+
+func (s *employeeService) AddPhoto(pb.EmployeeService_AddPhotoServer) error {
+	panic("implement me")
+}
+
+func (s *employeeService) SaveAll(pb.EmployeeService_SaveAllServer) error {
+	panic("implement me")
+}
+
 func (s *employeeService) GetByNo(context.Context, *pb.GetByNoRequest) (*pb.EmployeeResponse, error)  {
 	return  nil , nil
 }
 
-func (s *employeeService) GetAll(request *pb.GetAllRequest,server pb.EmployeeService_SaveAllServer) error  {
-	return  nil
-}
-
-func (s *employeeService)AddPhoto(pb.EmployeeService_SaveAllServer)  error {
-	return  nil
-}
 
 func (s *employeeService) Save (context.Context, *pb.EmployeeRequest)(*pb.EmployeeResponse,error) {
 	return  nil,nil
