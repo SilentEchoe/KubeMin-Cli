1fa5cfea5ffd1d1ff940f0b99d7ab91155ef0c24
kai_wang <871779972@qq.com>
2020-05-19 21:50:09 +0800

    grpc: client stream

--- grpc-client/main.go
+++ grpc-client/main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 	"grpc-client/pb"
 	"io"
 	"log"
+	"os"
 )
 
 const port = ":5001"
@@ -25,8 +27,56 @@ func main()  {
 	defer conn.Close()
 	client := pb.NewEmployeeServiceClient(conn)
 	//getByNo(client)
-	getAll(client)
+	//getAll(client)
+	addPhoto(client)
 }
+
+func addPhoto(client pb.EmployeeServiceClient)  {
+	imgFile ,err := os.Open("wlop.jpg")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer  imgFile.Close()
+
+	md := metadata.New(map[string]string{"no" : "1994"})
+
+	context  := context.Background()
+	context = metadata.NewOutgoingContext(context,md)
+
+	stream,err := client.AddPhoto(context)
+
+	if err!=nil {
+		log.Fatal(err.Error())
+	}
+
+	for {
+		chunk := make([]byte, 128*1024)
+		chunkSize ,err := imgFile.Read(chunk)
+
+		if err == io.EOF {
+			break
+		}
+
+		if err !=nil{
+			log.Fatal(err.Error())
+		}
+
+		if chunkSize < len(chunk) {
+			chunk = chunk[:chunkSize]
+		}
+		stream.Send(&pb.AddPhotoRequest{Data:chunk})
+
+	}
+
+	res ,err := stream.CloseAndRecv()
+	if err!=nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println(res.IsOk)
+
+}
+
 
 func getAll(client pb.EmployeeServiceClient)  {
 	stream , err := client.GetAll(context.Background(),&pb.GetAllRequest{})
