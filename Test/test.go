2e3d17c03e355dbaad18975ba249abc4b44755ac
kai_wang <871779972@qq.com>
2020-06-12 16:38:10 +0800

    API基本请求

--- Test/test.go
+++ Test/test.go
@@ -5,9 +5,12 @@ import (
 	"github.com/micro/go-micro/client/selector"
 	"github.com/micro/go-micro/registry"
 	"github.com/micro/go-plugins/registry/consul"
+	"io/ioutil"
 	"log"
+	"net/http"
 
 	"time"
+
 )
 
 func main()  {
@@ -29,10 +32,31 @@ func main()  {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(node.Id,node.Address,node.Metadata)
+
+		callRes,err := callAPI(node.Address,"/v1/prods","GET")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(callRes)
 		time.Sleep(time.Second*1)
 	}
 
 
+
+}
+
+
+func callAPI(addr string,path string,method string) (string,error){
+	req ,_ := http.NewRequest(method,"http://" + addr + path,nil)
+	client := http.DefaultClient
+	res,err := client.Do(req)
+	if err!=nil {
+		return  "",err
+	}
+
+	defer  res.Body.Close()
+	buf,err := ioutil.ReadAll(res.Body)
+	return  string(buf),nil
 
 }
