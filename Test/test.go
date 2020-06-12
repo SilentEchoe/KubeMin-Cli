b272c02b6bdd43114c3a921633405e4a2f455778
kai_wang <871779972@qq.com>
2020-06-12 15:10:11 +0800

    获取consul 的服务IP

---
+++ Test/test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/micro/go-micro/client/selector"
+	"github.com/micro/go-micro/registry"
+	"github.com/micro/go-plugins/registry/consul"
+	"log"
+)
+
+func main()  {
+	consulReg := consul.NewRegistry(
+		registry.Addrs("http://127.0.0.1:8500/"),
+	)
+	 getService,err := consulReg.GetService("prodservice")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 随机取
+	next := selector.Random(getService)
+	node,err :=  next()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(node.Id,node.Address,node.Metadata)
+
+}
