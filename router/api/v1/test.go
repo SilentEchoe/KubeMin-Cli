b5ae401bc6dd7037054f89ea42d18f473b2c1f33
kai_wang <871779972@qq.com>
2020-06-20 23:45:29 +0800

    更改测试方法

--- router/api/v1/test.go
+++ router/api/v1/test.go
@@ -1,23 +1,29 @@
 package v1
 
 import (
+	"LearningNotes-GoMicro/pkg/e"
+
 	"LearningNotes-GoMicro/Helper"
 	"LearningNotes-GoMicro/ProdService"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
+// @Summary 测试接口
+// @Produce  json
+// @Param name query int true "modelId"
+// @Success 200 {string} string "{"code":200,"data":{},"msg":"ok"}"
+// @Router /api/v1/GetConfigFiles [Get]
 func GetTestList (c *gin.Context)  {
 
 	data := make(map[string]interface{})
 	//maps := make(map[string]interface{})
 	code := e.ERROR
-
-		var pr Helper.ProdsRequest
-		err := c.Bind(&pr)
-		if err != nil || pr.Size <=0 {
-			pr= Helper.ProdsRequest{Size:2}
-		}
+	var pr Helper.ProdsRequest
+	err := c.Bind(&pr)
+	if err != nil || pr.Size <=0 {
+		pr= Helper.ProdsRequest{Size:2}
+	}
 	code = e.SUCCESS
 	ProdService.NewProdList(pr.Size)
 	c.JSON(http.StatusOK, gin.H{
--- router/router.go
+++ router/router.go
@@ -1,14 +1,14 @@
 package routers
 
 import (
-	"LearningNotes-Go/pkg/setting"
 
-	_ "LearningNotes-Go/docs"
+
+	// _ "LearningNotes-Go/docs"
 	"github.com/gin-gonic/gin"
-	"github.com/swaggo/gin-swagger"
-	"github.com/swaggo/gin-swagger/swaggerFiles"
+	//"github.com/swaggo/gin-swagger"
+	//"github.com/swaggo/gin-swagger/swaggerFiles"
 
-	"LearningNotes-Go/routers/api/v1"
+	"LearningNotes-GoMicro/routers/api/v1"
 )
 
 func InitRouter() *gin.Engine {
@@ -24,17 +24,8 @@ func InitRouter() *gin.Engine {
 	//jwt.JWT()
 	apiv1.Use()
 	{
-		//获取moduleName
-		apiv1.GET("/moduleNames", v1.GetModelNames)
-
-		//查询型号类型
-		apiv1.GET("/moduleTypes", v1.GetModelTypes)
-
 		//查询bin文件
-		apiv1.POST("/moduleBins", v1.GetModelBins)
-
-		//查询配置文件
-		apiv1.GET("/configFiles", v1.GetConfigFiles)
+		apiv1.POST("/test", v1.GetTestList)
 	}
 
 	return r
