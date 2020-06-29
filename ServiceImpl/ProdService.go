82403a23f88900bd9474f191199588bd93e13161
kai_wang <871779972@qq.com>
2020-06-29 15:39:00 +0800

    添加grpc 注入

--- ServiceImpl/ProdService.go
+++ ServiceImpl/ProdService.go
@@ -1,16 +1,27 @@
 package ServiceImpl
 
 import (
+
 	Service "LearningNotes-GoMicro/Services"
 	"context"
-	_ "LearningNotes-GoMicro/Services"
+	"strconv"
+
 )
 
 type  ProdService struct {
 
 }
+func (*ProdService) GetProdsList(ctx context.Context,in *Service.ProdsRequest,res *Service.ProdListResponse) error{
+	models := make([]*Service.ProdModel,0)
+	var i int32
+	for i= 0;i <in.Size;i++ {
+		models = append(models,newProd(100 + i,"prodname" + strconv.Itoa(100 +int(i))))
+	}
 
+	res.Data = models
+	return  nil
+}
 
-func (*ProdService) GetProdsList(ctx context.Context,int *Service.ProdsRequest,res *Service.ProdListResponse) error{
-	models := make([]*Service.pordModel,0)
+func newProd(id int32,pname string) *Service.ProdModel  {
+	return  &Service.ProdModel{ProdID: id,ProdName: pname}
 }
