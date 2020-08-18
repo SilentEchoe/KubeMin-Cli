9d3a3168575079ac9e1d2f49a0b56d56d891cdb4
kai_wang <871779972@qq.com>
2020-08-18 17:28:51 +0800

    添加统一调度测试

--- test/test.go
+++ test/test.go
@@ -1,1 +1,36 @@
-package test
+package main
+
+import (
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+
+	"fmt"
+)
+
+func main() {
+	c := sync.NewCond(&sync.Mutex{})
+	for i := 0; i < 10; i++ {
+		go listen(c)
+	}
+	time.Sleep(1*time.Second)
+	go broadcast(c)
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+}
+
+func broadcast(c *sync.Cond) {
+	c.L.Lock()
+	c.Broadcast()
+	c.L.Unlock()
+}
+
+func listen(c *sync.Cond) {
+	c.L.Lock()
+	c.Wait()
+	fmt.Println("listen")
+	c.L.Unlock()
+}
