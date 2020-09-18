1b37cdea270225deee2d4bf7c3a535f14d073964
kai_wang <871779972@qq.com>
2020-09-18 21:58:35 +0800

    初始化项目

--- main.go
+++ main.go
@@ -14,22 +14,17 @@ import (
 )
 
 func main() {
+
 	consulReg := consul.NewRegistry( //新建一个consul注册的地址，也就是我们consul服务启动的机器ip+端口
 		registry.Addrs("127.0.0.1:8500"),
 	)
 
-	setting.Setup()
-	gredis.Setup()
-	models.Setup()
-	logging.Setup()
+
 
 	ginRouter := routers.InitRouter()
 	httpServer := web.NewService(
-		//注册进consul服务中的service名字
-		web.Name("httpprodservice"),
-		//web.Handler()返回一个Option，我们直接把ginRouter穿进去，就可以和gin完美的结合
+		web.Name("HttpMainservice"),
 		web.Handler(ginRouter),
-		//注册进consul服务中的端口,也是这里我们gin的server地址
 		web.Address(":8000"),
 		web.Registry(consulReg),
 	)
