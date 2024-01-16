8bef204cda97474da97a84ff9bc28d764e4f8fb1
anonymity <kai.wang.info@gmail.com>
2024-01-16 16:31:05 +0800

    添加随机函数，以及添加Go mod

---
+++ pkg/apiserver/utils/random.go
@@ -0,0 +1,23 @@
+package utils
+
+import "math/rand"
+
+// RandomString 创建一个随机数
+func RandomString(n int) string {
+	var letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	result := make([]byte, n)
+	for i := range result {
+		result[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(result)
+}
+
+// RandomStringWithNumber 创建一个带数字的随机数
+func RandomStringWithNumber(n int) string {
+	var letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	result := make([]byte, n)
+	for i := range result {
+		result[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(result)
+}
