daa740c5e8801e14011480e310b6f49268f5d5d6
anonymity <33675262+SilentEchoe@users.noreply.github.com>
2024-12-31 10:58:55 +0800

    auth kubernetes client

--- pkg/apiserver/utils/auth.go
+++ pkg/apiserver/utils/auth.go
@@ -1,21 +1,57 @@
 package utils
 
 import (
+	"KubeMin-Cli/pkg/apiserver/domain/model"
 	"context"
+	"github.com/oam-dev/kubevela/pkg/features"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apiserver/pkg/authentication/user"
+	"k8s.io/apiserver/pkg/endpoints/request"
 	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+// KubeMinCliProjectGroupPrefix the prefix kubeMinCli project
+const KubeMinCliProjectGroupPrefix = "kubemincli:project:"
+const KubeMinCliClientGroup = "kubemincli:client"
+
+// UXDefaultGroup This group means directly using the original identity registered by the cluster.
+const UXDefaultGroup = "kubemincli:ux"
+
 type AuthClient struct {
-	kubeClient client.Client
+	client.Client
+}
+
+func ContextWithUserInfo(ctx context.Context) context.Context {
+	if !features.APIServerFeatureGate.Enabled(features.APIServerEnableImpersonation) {
+		return ctx
+	}
+	userInfo := &user.DefaultInfo{Name: user.Anonymous}
+	if username, ok := UsernameFrom(ctx); ok {
+		userInfo.Name = username
+	}
+	if project, ok := ProjectFrom(ctx); ok && project != "" {
+		userInfo.Groups = []string{KubeMinCliProjectGroupPrefix + project, KubeMinCliClientGroup}
+	} else {
+		userInfo.Groups = []string{UXDefaultGroup}
+	}
+	userRole, ok := UserRoleFrom(ctx)
+	if ok && features.APIServerFeatureGate.Enabled(features.APIServerEnableAdminImpersonation) {
+		for _, role := range userRole {
+			if role == model.RoleAdmin {
+				userInfo.Groups = []string{UXDefaultGroup}
+			}
+		}
+	}
+	return request.WithUser(ctx, userInfo)
 }
 
 func (c *AuthClient) Get(ctx context.Context, key client.ObjectKey, obj client.Object, opts ...client.GetOption) error {
-	//TODO implement me
-	panic("implement me")
+	// 根据上下文获取当前用户的信息
+	ctx = ContextWithUserInfo(ctx)
+	return c.Client.Get(ctx, key, obj)
 }
 
 func (c *AuthClient) List(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
