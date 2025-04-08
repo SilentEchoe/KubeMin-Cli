10f61deb7b92d930c141e5a4123f66b9d761dd7e
anonymity <33675262+SilentEchoe@users.noreply.github.com>
2025-04-08 14:01:03 +0800

    test:get deploy

---
+++ pkg/apiserver/event/workflow/job/job_service.go
@@ -0,0 +1,118 @@
+package job
+
+import (
+	"KubeMin-Cli/pkg/apiserver/config"
+	"KubeMin-Cli/pkg/apiserver/domain/model"
+	"context"
+	"fmt"
+	app "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/klog/v2"
+	"runtime/debug"
+	"sync"
+	"time"
+)
+
+type DeployJobCtl struct {
+	namespace string
+	job       *model.JobTask
+	client    *kubernetes.Clientset
+	ack       func()
+}
+
+func NewDeployJobCtl(job *model.JobTask, client *kubernetes.Clientset, ack func()) *DeployJobCtl {
+	return &DeployJobCtl{
+		job:    job,
+		client: client,
+		ack:    ack,
+	}
+}
+
+func runJob(ctx context.Context, job *model.JobTask, client *kubernetes.Clientset, ack func()) {
+	// 如果Job的状态为暂停或者跳过，则直接返回
+	if job.Status == config.StatusPassed || job.Status == config.StatusSkipped {
+		return
+	}
+	job.Status = config.StatusPrepare
+	job.StartTime = time.Now().Unix()
+	ack()
+
+	klog.Infof(fmt.Sprintf("start job: %s,status: %s", job.JobType, job.Status))
+	jobCtl := initJobCtl(job, client, ack)
+	defer func(jobInfo *JobCtl) {
+		if err := recover(); err != nil {
+			errMsg := fmt.Sprintf("job: %s panic: %v", job.Name, err)
+			klog.Errorf(errMsg)
+			debug.PrintStack()
+			job.Status = config.StatusFailed
+			job.Error = errMsg
+		}
+		job.EndTime = time.Now().Unix()
+		klog.Infof("finish job: %s,status: %s", job.Name, job.Status)
+		ack()
+		klog.Infof("updating job info into db...")
+		err := jobCtl.SaveInfo(ctx)
+		if err != nil {
+			klog.Errorf("update job info: %s into db error: %v", err)
+		}
+	}(&jobCtl)
+
+	// 执行对应的JOb任务
+	jobCtl.Run(ctx)
+}
+
+func (c *DeployJobCtl) Clean(ctx context.Context) {}
+
+// SaveInfo  创建Job的详情信息
+func (c *DeployJobCtl) SaveInfo(ctx context.Context) error {
+	return nil
+}
+
+func (c *DeployJobCtl) Run(ctx context.Context) {
+	c.job.Status = config.StatusRunning
+	c.ack() // 通知工作流开始运行
+	if err := c.run(ctx); err != nil {
+		return
+	}
+	//这里是部署完毕后，将状态进行同步
+	c.wait(ctx)
+}
+
+func (c *DeployJobCtl) run(ctx context.Context) error {
+	if c.client == nil {
+		panic("client is nil")
+	}
+
+	var deploy *app.Deployment
+	if d, ok := c.job.JobInfo.(*app.Deployment); ok {
+		deploy = d
+	} else {
+		return fmt.Errorf("deploy Job Job.Info Conversion type failure")
+	}
+
+	result, err := c.client.AppsV1().Deployments("default").Create(ctx, deploy, metav1.CreateOptions{})
+	if err != nil {
+		klog.Errorf(err.Error())
+		return err
+	}
+	klog.Infof("JobTask Deploy Successfully %q.\n", result.GetObjectMeta().GetName())
+
+	// 将这个任务标记为已完成
+	c.job.Status = config.StatusCompleted
+	c.ack()
+
+	// TODO 这里可能需要记录这个Job
+
+	return nil
+}
+
+func (c *DeployJobCtl) updateServiceModuleImages(ctx context.Context) error {
+	wg := sync.WaitGroup{}
+	wg.Wait()
+	return nil
+}
+
+func (c *DeployJobCtl) wait(ctx context.Context) {
+
+}
--- pkg/test/test.go
+++ pkg/test/test.go
@@ -1,1 +1,52 @@
-package test
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+)
+
+func main() {
+
+	var loadKubeClient *string
+	if home := homedir.HomeDir(); home != "" {
+		// 如果输入了kubeconfig参数，该参数的值就是kubeconfig文件的绝对路径，
+		// 如果没有输入kubeconfig参数，就用默认路径~/.kube/config
+		loadKubeClient = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	}
+
+	if loadKubeClient == nil {
+		fmt.Errorf("please call SetKubeConfig first")
+		return
+	}
+	loadConf, err := clientcmd.BuildConfigFromFlags("", *loadKubeClient)
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(loadConf)
+
+	deploymentName := "first-app-front"
+	namespace := "default"
+
+	// 获取 Deployment 对象
+	deploy, err := clientset.AppsV1().Deployments(namespace).Get(
+		context.TODO(),
+		deploymentName,
+		metav1.GetOptions{},
+	)
+	if err != nil {
+		panic(fmt.Errorf("failed to get Deployment: %v", err))
+	}
+
+	fmt.Printf("Deployment: %s\n", deploymentName)
+	fmt.Printf("Desired Replicas: %d\n", *deploy.Spec.Replicas)
+	fmt.Printf("Current Replicas: %d\n", deploy.Status.Replicas)
+	fmt.Printf("Available Replicas: %d\n", deploy.Status.AvailableReplicas)
+	fmt.Printf("Ready Replicas: %d\n", deploy.Status.ReadyReplicas)
+}
