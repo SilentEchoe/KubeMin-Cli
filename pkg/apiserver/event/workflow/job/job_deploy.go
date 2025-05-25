e3c5685e8e8bb4a7face517f2bff6d6a5ccedf37
anonymity <33675262+SilentEchoe@users.noreply.github.com>
2025-05-25 19:56:16 +0800

    refactor: jobGenerate

--- pkg/apiserver/event/workflow/job/job_deploy.go
+++ pkg/apiserver/event/workflow/job/job_deploy.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	app "k8s.io/api/apps/v1"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
--- pkg/apiserver/event/workflow/job/job_service.go
+++ pkg/apiserver/event/workflow/job/job_service.go
@@ -9,6 +9,7 @@ import (
 	"sync"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
